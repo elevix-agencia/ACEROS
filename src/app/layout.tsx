f743e69381330bb0a20b80a7fc0dbf268813894f
Renato Coutinho <futcout@gmail.com>
2026-08-11 00:24:44 +0000

    # INSERÇÃO DEFINITIVA DO GOOGLE TAG MANAGER (GTM)
    Você é um desenvolved

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-XXXXXXX');`,
+            })(window,document,'script','dataLayer','GTM-WMKKZ3CL');`,
           }}
         />
         {/* End Google Tag Manager */}
@@ -43,7 +43,7 @@ export default function RootLayout({
         {/* Google Tag Manager (noscript) */}
         <noscript
           dangerouslySetInnerHTML={{
-            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-XXXXXXX"
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WMKKZ3CL"
             height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
           }}
         />
